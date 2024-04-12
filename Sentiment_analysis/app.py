8a2726fa9d79917ea24e8534e1add640fa3f1041
veratinous <veratinous@gmail.com>
2024-04-12 11:37:45 +0100

    two updates

--- Sentiment_analysis/app.py
+++ Sentiment_analysis/app.py
@@ -5,13 +5,11 @@ import joblib
 app = Flask(__name__, static_url_path='/static')
 
 
-model = joblib.load('model\naive_bayes.pkl')
+model = joblib.load('model/naive_bayes.pkl')
 
 
 def preprocess_text(text):
-    # Convert text to lowercase
     text = text.lower()
-    # Remove special characters, punctuation, and extra whitespaces
     text = re.sub(r'[^a-zA-Z0-9\s]', '', text)
     text = re.sub(r'\s+', ' ', text)
     return text
@@ -19,22 +17,20 @@ def preprocess_text(text):
 
 @app.route('/')
 def home():
-    render_template('home.html')
+    return render_template('home.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        
         review = request.form['review']
-        # Preprocess the input review
         processed_review = preprocess_text(review)
-        # Use the best model to predict the sentiment
-        prediction = best_model.predict([processed_review])[0]
-        # Map prediction to sentiment label
+        prediction = model.predict([processed_review])[0]
         sentiment = "Positive" if prediction == 'Positive' else "Negative"
         return render_template('results.html', sentiment=sentiment, review=review)
+    else:
+        return render_template('home.html')
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0")
